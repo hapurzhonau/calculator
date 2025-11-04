c1adc79f50422cb7ba7358d84a85954695c8b56c
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2025-11-04 13:07:05 +0300

    chore: test husky

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import unicorn from 'eslint-plugin-unicorn';
+
+export default [
+  {
+    ignores: ['node_modules', 'dist', '.husky'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    plugins: {
+      unicorn,
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'no-console': 'warn',
+      'unicorn/filename-case': [
+        'error',
+        {
+          case: 'kebabCase',
+        },
+      ],
+      'unicorn/prevent-abbreviations': 'error',
+      'unicorn/prefer-dom-node-append': 'error',
+      'unicorn/prefer-dom-node-remove': 'error',
+      'unicorn/prefer-module': 'error',
+      'unicorn/prefer-node-protocol': 'error',
+      'unicorn/prefer-query-selector': 'error',
+      'unicorn/prefer-string-slice': 'error',
+      'unicorn/prefer-set-has': 'error',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: false,
+        },
+      },
+    },
+  },
+];
